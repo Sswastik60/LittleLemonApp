f37d155c6caffee6faa634a482844cc588a58d6e
Sswastik60 <sswastik60@gmail.com>
2026-06-28 08:51:13 +0530

    Navigation setup

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 // npx create-expo-app@latest WealthOne --template blank use this to create a new expo app
+
+// npm install @react-navigation/native use this to install react navigation
+
+// npx expo install react-native-screens react-native-safe-area-context use this to install react native screens and safe area context
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
